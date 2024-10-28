09342e34db89fc88b3762de39c383fcae6b9bbc2
lipan <lipanre@qq.com>
2024-10-28 20:52:10 +0800

    feat: 项目初始化提交

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+// 微信支付sdk
+group = "com.lipanre.sdk"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
