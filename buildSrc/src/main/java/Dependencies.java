fb843ed29e81eab6a8b68f416ddf443c64c5cee0
lipan <lipanre@qq.com>
2024-10-29 23:28:35 +0800

    feat: buildSrc模块增加依赖管理接口

--- buildSrc/src/main/java/Dependencies.java
+++ buildSrc/src/main/java/Dependencies.java
@@ -3,7 +3,10 @@
  *
  * @author lipanre
  */
-public class Dependencies {
+public interface Dependencies {
 
-
+    /**
+     * 微信http-client-sdk
+     */
+    String WECHAT_PAY_V3_HTTP_CLIENT_SDK = "com.github.wechatpay-apiv3:wechatpay-apache-httpclient:0.4.9";
 }
