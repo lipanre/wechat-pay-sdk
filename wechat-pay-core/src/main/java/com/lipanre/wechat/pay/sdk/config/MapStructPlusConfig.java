5ff586c2325a19d100e678710781219b98e7778a
lipan <lipanre@qq.com>
2024-11-01 23:07:07 +0800

    feat: 增加mapstructplus配置类

---
+++ wechat-pay-core/src/main/java/com/lipanre/wechat/pay/sdk/config/MapStructPlusConfig.java
@@ -0,0 +1,10 @@
+package com.lipanre.wechat.pay.sdk.config;
+
+import io.github.linpeilie.annotations.ComponentModelConfig;
+
+/**
+ * @author lipanre
+ */
+@ComponentModelConfig(componentModel = "default")
+public class MapStructPlusConfig {
+}
