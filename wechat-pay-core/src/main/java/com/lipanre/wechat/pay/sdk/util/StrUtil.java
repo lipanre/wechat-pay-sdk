bab145fd69acc4d525038c6abffbc84b8432da0f
lipan <lipanre@qq.com>
2024-11-02 01:31:02 +0800

    feat: 新增字符串处理工具类

---
+++ wechat-pay-core/src/main/java/com/lipanre/wechat/pay/sdk/util/StrUtil.java
@@ -0,0 +1,36 @@
+package com.lipanre.wechat.pay.sdk.util;
+
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 字符串工具类
+ *
+ * @author lipanre
+ */
+public class StrUtil {
+
+    private static final Matcher matcher = Pattern.compile("([A-Z])").matcher("");
+
+    /**
+     * 替换正则表达式
+     */
+    public static final String REPLACEMENT_REGEXP = "_$1";
+
+    /**
+     * 驼峰转下划线
+     *
+     * @param str 需要转换的字符串
+     * @return 下划线字符串
+     */
+    public static String toUnderLine(String str) {
+        matcher.reset(str);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(sb, REPLACEMENT_REGEXP);
+        }
+        matcher.appendTail(sb);
+        return sb.toString().toLowerCase(Locale.CHINA);
+    }
+}
