1182f3fe0432046ce4fe30bb599f21db9ed075fe
lipanre <lipanre@qq.com>
2024-11-06 08:58:53 +0800

    feat: 更新构建文件

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
-import org.jreleaser.gradle.plugin.JReleaserExtension
-import org.jreleaser.model.JReleaserException
-import org.jreleaser.model.Signing
 
-plugins {
-    id("org.jreleaser") version "1.15.0" apply false
-}
+//plugins {
+//    id("org.jreleaser") version "1.15.0" apply false
+//}
 
 buildscript {
 
@@ -52,7 +49,7 @@ subprojects {
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
     apply(plugin = "maven-publish")
-    apply(plugin = "org.jreleaser")
+//    apply(plugin = "org.jreleaser")
 
     configure<JavaPluginExtension> {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -61,75 +58,75 @@ subprojects {
         withSourcesJar()
     }
 
-    configure<PublishingExtension> {
-        publications {
-            create<MavenPublication>("mavenJava") {
-                from(components["java"])
-                groupId = project.group.toString()
-                artifactId = project.name
-                version = project.version.toString()
-
-                pom {
-                    name = "app"
-                    description = "Sample application"
-                    url = "https://github.com/aalmiray/app"
-                    inceptionYear = "2021"
-
-                    licenses {
-                        license {
-                            name = "Apache-2.0"
-                            url = "https://spdx.org/licenses/Apache-2.0.html"
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id = "MainTainer"
-                            name = "lipanre"
-                        }
-                    }
-
-                    scm {
-                        connection = "scm:git:https://github.com/aalmiray/app.git"
-                        developerConnection = "scm:git:ssh://github.com/aalmiray/app.git"
-                        url = "http://github.com/aalmiray/app"
-                    }
-                }
-                versionMapping {
-                    usage("java-api") {
-                        fromResolutionOf("runtimeClasspath")
-                    }
-                    usage("java-runtime") {
-                        fromResolutionResult()
-                    }
-                }
-            }
-        }
-
-        repositories {
-            maven {
-                url = uri(layout.buildDirectory.dir("staging-deploy"))
-            }
-        }
-    }
-
-    configure<JReleaserExtension> {
-        signing {
-            setActive("ALWAYS")
-            setMode("FILE")
-            armored = true
-            publicKey = "/Users/hanqf/develop_soft/gpg_key/hanqf/public.pgp"
-            secretKey = "/Users/hanqf/develop_soft/gpg_key/hanqf/private.pgp"
-        }
-
-        deploy {
-            maven {
-                mavenCentral {
-                    setActive("ALWAYS")
-                }
-            }
-        }
-    }
+//    configure<PublishingExtension> {
+//        publications {
+//            create<MavenPublication>("mavenJava") {
+//                from(components["java"])
+//                groupId = project.group.toString()
+//                artifactId = project.name
+//                version = project.version.toString()
+//
+//                pom {
+//                    name = "app"
+//                    description = "Sample application"
+//                    url = "https://github.com/aalmiray/app"
+//                    inceptionYear = "2021"
+//
+//                    licenses {
+//                        license {
+//                            name = "Apache-2.0"
+//                            url = "https://spdx.org/licenses/Apache-2.0.html"
+//                        }
+//                    }
+//
+//                    developers {
+//                        developer {
+//                            id = "MainTainer"
+//                            name = "lipanre"
+//                        }
+//                    }
+//
+//                    scm {
+//                        connection = "scm:git:https://github.com/aalmiray/app.git"
+//                        developerConnection = "scm:git:ssh://github.com/aalmiray/app.git"
+//                        url = "http://github.com/aalmiray/app"
+//                    }
+//                }
+//                versionMapping {
+//                    usage("java-api") {
+//                        fromResolutionOf("runtimeClasspath")
+//                    }
+//                    usage("java-runtime") {
+//                        fromResolutionResult()
+//                    }
+//                }
+//            }
+//        }
+//
+//        repositories {
+//            maven {
+//                url = uri(layout.buildDirectory.dir("staging-deploy"))
+//            }
+//        }
+//    }
+//
+//    configure<JReleaserExtension> {
+//        signing {
+//            setActive("ALWAYS")
+//            setMode("FILE")
+//            armored = true
+//            publicKey = "/Users/hanqf/develop_soft/gpg_key/hanqf/public.pgp"
+//            secretKey = "/Users/hanqf/develop_soft/gpg_key/hanqf/private.pgp"
+//        }
+//
+//        deploy {
+//            maven {
+//                mavenCentral {
+//                    setActive("ALWAYS")
+//                }
+//            }
+//        }
+//    }
 
 
     val implementation by configurations
@@ -141,29 +138,29 @@ subprojects {
     }
 
 
-//    afterEvaluate {
-//        if (!project.name.endsWith("-example")) {
-//            project.extensions.getByType<PublishingExtension>().apply {
-//                publications {
-//                    create<MavenPublication>("mavenJava") {
-//                        from(components["java"])
-//                        groupId = project.group.toString()
-//                        artifactId = project.name
-//                        version = project.version.toString()
-//                    }
-//                }
-//                repositories {
-//                    maven {
-//                        url = uri("https://packages.aliyun.com/6721ee46cc7f222f53c841da/maven/2500548-snapshot-ycars7")
-//                        credentials {
-//                            username = "662367769bf402bca06965e4"
-//                            password = "WbQuvKQ[oOSl"
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
+    afterEvaluate {
+        if (!project.name.endsWith("-example")) {
+            project.extensions.getByType<PublishingExtension>().apply {
+                publications {
+                    create<MavenPublication>("mavenJava") {
+                        from(components["java"])
+                        groupId = project.group.toString()
+                        artifactId = project.name
+                        version = project.version.toString()
+                    }
+                }
+                repositories {
+                    maven {
+                        url = uri("https://packages.aliyun.com/6721ee46cc7f222f53c841da/maven/2500548-snapshot-ycars7")
+                        credentials {
+                            username = "662367769bf402bca06965e4"
+                            password = "WbQuvKQ[oOSl"
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
 
