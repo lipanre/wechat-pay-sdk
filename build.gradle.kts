ab13212c3a032b9420428bfe11c506d73c7aeb81
lipanre <lipanre@qq.com>
2025-02-11 16:44:56 +0800

    fix: 将Java兼容性版本从21更改为17

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,8 @@ subprojects {
 //    apply(plugin = "org.jreleaser")
 
     configure<JavaPluginExtension> {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
 //        withJavadocJar()
         withSourcesJar()
     }
