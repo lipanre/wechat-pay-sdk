d8bcda6da0cc6dbc363ef3c9899edcb3ff5d41a5
lipan <lipanre@qq.com>
2025-04-04 00:03:30 +0800

    feat: 增加发布到maven中央仓库配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
-import kotlin.math.sign
-
+import com.vanniktech.maven.publish.SonatypeHost
 plugins {
     `java-library`
+    id("com.vanniktech.maven.publish") version "0.31.0"
 }
+
 
 buildscript {
 
@@ -51,7 +52,9 @@ subprojects {
     apply(plugin = "io.freefair.lombok")
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "com.vanniktech.maven.publish")
     apply(plugin = "signing")
+
 
     configure<JavaPluginExtension> {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -66,6 +69,54 @@ subprojects {
         implementation(Dependencies.MAP_STRUCT)
         annotationProcessor(Dependencies.MAP_STRUCT_PROCESSOR)
     }
+
+    if (!project.name.endsWith("example")) {
+        mavenPublishing {
+            // Define coordinates for the published artifact
+            coordinates(
+                groupId = "io.github.lipanre",
+                artifactId = project.name,
+                version = "1.0.0"
+            )
+
+            // Configure POM metadata for the published artifact
+            pom {
+                name.set("WeChat Pay SDK")
+                description.set("Wechat http v3 Pay SDK")
+                inceptionYear.set("2025")
+                url.set("https://github.com/lipanre/wechat-pay-sdk")
+
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                // Specify developers information
+                developers {
+                    developer {
+                        id.set("lipanre")
+                        name.set("lipanre")
+                        email.set("lipanre@qq.com")
+                    }
+                }
+
+                // Specify SCM information
+                scm {
+                    url.set("https://github.com/lipanre/wechat-pay-sdk")
+                }
+            }
+
+            // Configure publishing to Maven Central
+            publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+            // Enable GPG signing for all publications
+            signAllPublications()
+        }
+
+    }
+
 }
 
 
